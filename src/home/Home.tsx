284f56f6eb90f152772225b536cfe967b8f8eace
David <davidpchi@gmail.com>
2022-09-15 11:21:20 -0700

    Flesh out the navigation more, add home page

---
+++ src/home/Home.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <div style={{display: "flex", minHeight: "100vh", backgroundColor: "#282c34", justifyContent: "center", alignItems: "center"}}>
+        <h1 style={{color: "white"}}> Welcome to Monday Night Customs Hub </h1>
+    </div>
+}
---
+++ src/navigation/Root.tsx
@@ -0,0 +1,97 @@
+import { useCallback, useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
+
+import "./Root.css";
+import CSS from 'csstype';
+
+const baseSideBarStyle: CSS.Properties = {
+  height: "100%", /* 100% Full-height */
+  position: "fixed", /* Stay in place */
+  zIndex: 1, /* Stay on top */
+  top: 0, /* Stay at the top */
+  left: 0,
+  backgroundColor: "#111", /* Black*/
+  overflowX: "hidden", /* Disable horizontal scroll */
+  paddingTop: "60px", /* Place content 60px from the top */
+  transition: "0.5s", /* 0.5 second transition effect to slide in the sidenav */
+}
+
+const sideBarStyle: CSS.Properties = {
+  ...baseSideBarStyle,
+  width: "250px", /* 0 width - change this with JavaScript */
+}
+
+const sideBarCollapsedStyle: CSS.Properties = {
+  ...baseSideBarStyle,
+  width: "0px", /* 0 width - change this with JavaScript */
+}
+
+const openMenuIcon: CSS.Properties = {
+  flex: 1,
+  fontSize: "36px",
+  marginLeft: "50px",
+  paddingTop: "16px",
+  textDecoration: "none",
+  color: "#818181",
+  transition: "0.3s",
+  justifyContent: "center",
+  alignItems: "center",
+  alignSelf: "center",
+  textAlign: "center",
+  maxWidth: "64px",
+  borderWidth: 0,
+  backgroundColor: "transparent",
+}
+
+const sideBarClose: CSS.Properties = {
+  position: "absolute",
+  top: 0,
+  right: "25px",
+  fontSize: "36px",
+  marginLeft: "50px",
+  padding: "8px 8px 8px 32px",
+  textDecoration: "none",
+  color: "#818181",
+  display: "block",
+  transition: "0.3s",
+  borderWidth: 0,
+  backgroundColor: "transparent",
+}
+
+function NavigationItem({route, label}: {route: string, label: string}) {
+  const navigate = useNavigate();
+  const onClick = useCallback(()=> {
+    navigate(route)
+  },[route, navigate]);
+  return <button onClick={onClick} style={{backgroundColor: "transparent", borderWidth: 0 }}>
+    <a>{label}</a>
+  </button>
+}
+
+export default function Root() {
+  const [isNavigationCollapsed, setIsNavigationCollapsed] = useState<boolean>(true);
+
+    const onToggleNavigation = () => {
+      setIsNavigationCollapsed(!isNavigationCollapsed);
+    }
+
+    return (
+    <>
+      <div id="mySidenav" className="sidenav" style={isNavigationCollapsed ? sideBarCollapsedStyle : sideBarStyle}>
+        <button onClick={onToggleNavigation} style={sideBarClose}><a><AiOutlineClose/></a></button>
+        <NavigationItem label="Home" route="/"/>
+        <NavigationItem label="Player Overview" route="/playerOverview"/>
+        <NavigationItem label="Matchmaker" route="/matchmaker"/>
+      </div>
+      <div style={{display: "flex", flex:1, width: "100%", flexDirection: "column"}}>
+        <div style={{display: "flex", flex: 1, flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+          <button style={openMenuIcon} onClick={onToggleNavigation}><a><AiOutlineMenu/></a></button>
+          <h1 style={{flex: 1, maxWidth: 500,}}>Monday Night Customs Hub</h1>
+        </div>
+        <Outlet />
+      </div>
+    </>
+    );
+  }
