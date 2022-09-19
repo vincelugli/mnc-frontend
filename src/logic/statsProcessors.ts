e59927652f1e6b0b9f7d455e365987ec0529b31b
Michael Valdes <michael.j.valdes@gmail.com>
2022-09-18 20:10:51 -0400

    Add new SortableTable and refactor PlayerOverview
    The new SortableTable uses the newer version of react-table. This is a
    WIP and will eventually replace the previous SortableTable.

--- src/logic/statsProcessors.ts
+++ src/logic/statsProcessors.ts
@@ -1,3 +1,4 @@
+import { PlayerTableData } from "./../types/domain/Player";
 import { Champion } from "../types/domain/Champion";
 import { Player } from "../types/domain/Player";
 
@@ -5,32 +6,37 @@ import { Player } from "../types/domain/Player";
  * Given a collection of players, map to a collection of players with processed stats
  * @param data A collection of playeres to process
  */
-export function processPlayers(data: Player[] | undefined) {
-    return data ? (data.map((player) => {
-      const wins = player.wins ?? 0;
-      const losses = player.losses ?? 0;
-      const totalGames = wins + losses;
+export const processPlayers = (
+  data: Player[] | undefined
+): PlayerTableData[] => {
+  return data
+    ? data.map((player) => {
+        const wins = player.wins ?? 0;
+        const losses = player.losses ?? 0;
+        const totalGames = wins + losses;
+        console.log(totalGames);
 
-      return {
-        ...player,
-        winPercentage: Math.round(wins / totalGames * 100) + "%",
-        totalGames: totalGames,
-        mmr: Math.round(player.mmr ?? 0)
-      }
-    })) : [];
-  }
-  
+        return {
+          ...player,
+          winPercentage: Math.round((wins / totalGames) * 100) + "%",
+          totalGames: totalGames,
+          mmr: Math.round(player.mmr ?? 0),
+        };
+      })
+    : [];
+};
+
 /**
  * Given a collection of players, map to a key value pair of championName to champion that has stats centered around that champion
- * @param data 
+ * @param data
  */
 export function processChampions(data: Player[] | undefined) {
-  const championMap: {[key: string]: Champion} = {};
+  const championMap: { [key: string]: Champion } = {};
   if (data) {
     for (const player of data) {
       // iterate through the player's champions to aggregate champion info
       if (player.champions) {
-        for (const [key,value] of Object.entries(player.champions)) {
+        for (const [key, value] of Object.entries(player.champions)) {
           if (championMap[key] === undefined) {
             // champion does not exist in our collection, so we can add it
             championMap[key] = {
@@ -38,16 +44,19 @@ export function processChampions(data: Player[] | undefined) {
               losses: value.losses,
               wins: value.wins,
               totalGames: value.losses + value.wins,
-              winPercentage: value.winPercentage
-            }
+              winPercentage: value.winPercentage,
+            };
           } else {
             championMap[key] = {
               name: key,
               losses: championMap[key].losses + value.losses,
               wins: championMap[key].wins + value.wins,
-              winPercentage: Math.round((championMap[key].winPercentage + value.winPercentage) / 2),
-              totalGames: championMap[key].totalGames + value.losses + value.wins
-            }
+              winPercentage: Math.round(
+                (championMap[key].winPercentage + value.winPercentage) / 2
+              ),
+              totalGames:
+                championMap[key].totalGames + value.losses + value.wins,
+            };
           }
         }
       }
--- src/types/domain/Player.ts
+++ src/types/domain/Player.ts
@@ -1,17 +1,26 @@
 export type PlayerChampionData = {
-    name: string;
-    totalGames: number;
-    wins: number;
-    losses: number;
-    winPercentage: number;
-}
+  name: string;
+  totalGames: number;
+  wins: number;
+  losses: number;
+  winPercentage: number;
+};
 
 export type Player = {
-    name: string;
-    wins?: number;
-    losses?: number;
-    mmr?: number;
-    champions?: {
-        [key: string]: PlayerChampionData
-    }
-}
+  name: string;
+  wins?: number;
+  losses?: number;
+  mmr?: number;
+  champions?: {
+    [key: string]: PlayerChampionData;
+  };
+};
+
+export type PlayerTableData = {
+  name: string;
+  wins?: number | undefined;
+  winPercentage: string;
+  losses?: number;
+  totalGames: number;
+  mmr?: number;
+};
