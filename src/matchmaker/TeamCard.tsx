ed94068783dc6b1d74d6b32cf7b5d7684f4a03a3
Michael Valdes <michael.j.valdes@gmail.com>
2022-09-21 01:30:08 -0400

    Add ESLint

---
+++ src/matchmaker/TeamCard.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import { Player } from "../types/domain/Player";
+
+const TeamCard = (team: Player[]) => {
+  return <Box></Box>;
+};
+
+export default TeamCard;
