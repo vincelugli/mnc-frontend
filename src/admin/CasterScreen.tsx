1cbfe9ad08c383c77f45f8892d5f9b961764e8e8
David <davidpchi@gmail.com>
2022-09-28 17:35:40 -0700

    Add more tools

--- src/admin/CasterScreen.tsx
+++ src/admin/CasterScreen.tsx
@@ -17,12 +17,17 @@ export const CasterScreen = React.memo(function CasterScreen() {
     const videoId = useLoaderData() as string;
     const canvasRef = useRef<ReactSketchCanvasRef>(null);
     const [drawColor, setDrawColor] = useState(CasterColor.Red);
+    const [showCanvas, setShowCanvas] = useState(true);
 
     const onClear = useCallback(() => {
         if (canvasRef.current !== null) {
             canvasRef.current.clearCanvas();
         }
     }, [canvasRef]);
+
+    const toggleCanvas = useCallback(() => {
+        setShowCanvas(!showCanvas);
+    }, [showCanvas]);
 
     const onBlue = () => {
         setDrawColor(CasterColor.Blue);
@@ -42,18 +47,30 @@ export const CasterScreen = React.memo(function CasterScreen() {
                 backgroundColor: '#282c34',
             }}
         >
-            <div style={{ flex: 1, zIndex: 10, height: 50, marginTop: 0 }}>
-                <Button onClick={onBlue} style={{}}>
-                    🟦
+            <div
+                style={{
+                    flex: 1,
+                    zIndex: 10,
+                    height: 50,
+                    marginTop: 0,
+                    flexWrap: 'nowrap',
+                }}
+            >
+                <Button onClick={onBlue}>🟦</Button>
+                <Button onClick={onRed}>🟥</Button>
+                <Button onClick={onGreen}>🟩</Button>
+                <Button onClick={onClear}>CLEAR</Button>
+                <Button onClick={toggleCanvas}>
+                    {showCanvas ? 'HIDE CANVAS' : 'SHOW CANVAS'}
                 </Button>
-                <Button onClick={onRed} style={{}}>
-                    🟥
-                </Button>
-                <Button onClick={onGreen} style={{}}>
-                    🟩
-                </Button>
-                <Button onClick={onClear} style={{}}>
-                    CLEAR
+                <Button
+                    style={{
+                        backgroundColor: showCanvas
+                            ? undefined
+                            : 'rgb(0,0,0,0.5)',
+                    }}
+                >
+                    {showCanvas ? 'Canvas Visible' : 'Canvas Hidden'}
                 </Button>
             </div>
             <div style={{ flex: 1 }}>
@@ -73,34 +90,35 @@ export const CasterScreen = React.memo(function CasterScreen() {
                         src={`https://www.youtube.com/embed/${videoId}?rel=0`}
                     />
                 </div>
-                <div
-                    style={{
-                        position: 'absolute',
-                        right: 0,
-                        left: 0,
-                        bottom: 45,
-                        top: 0,
-                    }}
-                >
+                {showCanvas === true ? (
                     <div
                         style={{
-                            display: 'flex',
-                            width: '100%',
-                            height: '100%',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            //backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                            position: 'absolute',
+                            right: 0,
+                            left: 0,
+                            bottom: 45,
+                            top: 0,
                         }}
                     >
-                        <ReactSketchCanvas
-                            ref={canvasRef}
-                            style={{ flex: 1, alignSelf: 'stretch' }}
-                            canvasColor={'rgb(0,0,0,0)'}
-                            strokeWidth={4}
-                            strokeColor={drawColor}
-                        />
+                        <div
+                            style={{
+                                display: 'flex',
+                                width: '100%',
+                                height: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <ReactSketchCanvas
+                                ref={canvasRef}
+                                style={{ flex: 1, alignSelf: 'stretch' }}
+                                canvasColor={'rgb(0,0,0,0)'}
+                                strokeWidth={4}
+                                strokeColor={drawColor}
+                            />
+                        </div>
                     </div>
-                </div>
+                ) : null}
             </div>
         </div>
     );
